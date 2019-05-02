ab0d36cbb4d6cdcc291041d9c7223b6405ea189d
erickgjs99 <erickjs99lol@gmail.com>
2019-05-02 14:43:47 -0500

    ejercicio clase jueves

--- miproyecto/run_4.py
+++ miproyecto/run_4.py
@@ -7,11 +7,31 @@ ciclo_uni = 1200
 segu_ciclo = 0
 #Preguntar al usuario
 edad = input("¿Cuantos años tienes? \n")
-modalidad = input("Estudias en modalidad a Distancia \n")
+modalidad = input("Estudias en modalidad a Distancia \n 1._Si \n 2._No \n")
 #Factores a determinar para el cobro
 edad = int(edad) 
-if modalidad = "si" & edad <= 20:
-    segu_ciclo = 100
-    calc_uni = (ciclo_uni * 10)+(segu_ciclo*10)
-    print("El precio a pagar por su carrera a distancia sale un costo de $ %d" % (calc_uni))
+modalidad = int(modalidad)
 
+#Si es de modalidad a distancia y es menor a 20 años
+if (modalidad == 1 & edad <= 20):
+    segu_ciclo = 100
+    calc_uni = (ciclo_uni * 10)+(segu_ciclo * 10)
+    print("El precio a pagar por su carrera a distancia sale un costo de $ %d" % (calc_uni),"màs seguro incluido")
+
+#Si es de modalidad a distancia y es mayor a 20 años    
+if (modalidad == 1 & edad >= 20):
+    segu_ciclo_2 = 150
+    calc_uni = (ciclo_uni * 10)+(segu_ciclo * 10)
+    print("El precio a pagar por su carrera a distancia sale un costo de $ %d" % (calc_uni),"màs seguro incluido")
+
+#Si es de modalidad presencial y es menor a 20 años
+if (modalidad == 2 & edad <= 20):
+    segu_ciclo = 100
+    calc_uni = (ciclo_uni * 8)+(segu_ciclo * 8)
+    print("El precio a pagar por su carrera presencial sale un costo de $ %d" % (calc_uni),"màs seguro incluido")
+
+#Si es de modalidad presencial y es mayor a 20 años
+if (modalidad == 1 & edad > 20):
+    segu_ciclo_2 = 150
+    calc_uni = (ciclo_uni * 8)+(segu_ciclo * 8)
+    print("El precio a pagar por su carrera presencial sale un costo de $ %d" % (calc_uni),"màs seguro incluido")
